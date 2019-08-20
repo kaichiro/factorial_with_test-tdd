859f885563547a2500d7d4a54ac6e17965ebe943
kaichiro <kcfg23@gmail.com>
2019-08-20 17:02:28 -0300

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using factorial_with_test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Factorial_2()
+        {
+            Factorial factorial = new Factorial();
+            int number = 2;
+            int numberExpected = 2;
+            int numberActual = factorial.getFactorialNumber(number);
+            Assert.AreEqual(numberExpected, numberActual);
+        }
+
+        [TestMethod]
+        public void Factorial_3()
+        {
+            Factorial factorial = new Factorial();
+            int number = 3;
+            int numberExpected = 6;
+            int numberActual = factorial.getFactorialNumber(number);
+            Assert.AreEqual(numberExpected, numberActual);
+        }
+    }
+}
---
+++ factorial_with_test/Factorial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace factorial_with_test
+{
+    public class Factorial
+    {
+        public int getFactorialNumber(int number)
+        {
+            return
+                number == 0
+                ? 1
+                : number * getFactorialNumber(number - 1);
+        }
+    }
+}
